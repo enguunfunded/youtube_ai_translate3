aec461c4cd42446b40a3d095a52a3dba0a40fac3
enguunfunded <jagaa_tsg@yahoo.com>
2025-07-05 23:35:15 +0800

    Update translate.py
    ← Англиас → Монгол орчуулна

--- translate.py
+++ translate.py
@@ -1,13 +1,6 @@
-from transformers import MarianMTModel, MarianTokenizer
+from googletrans import Translator
 
 def translate_text(text):
-    src = "en"
-    tgt = "mn"
-    model_name = f'Helsinki-NLP/opus-mt-{src}-{tgt}'
-    tokenizer = MarianTokenizer.from_pretrained(model_name)
-    model = MarianMTModel.from_pretrained(model_name)
-
-    inputs = tokenizer([text], return_tensors="pt", truncation=True, max_length=512)
-    translated = model.generate(**inputs)
-    result = tokenizer.decode(translated[0], skip_special_tokens=True)
-    return result
+    translator = Translator()
+    translated = translator.translate(text, src='en', dest='mn')
+    return translated.text
