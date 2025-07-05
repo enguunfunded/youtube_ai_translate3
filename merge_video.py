ae245854d9d119103ad059ac47d9f8157c0cbcce
enguunfunded <jagaa_tsg@yahoo.com>
2025-07-05 23:19:48 +0800

    Create merge_video.py
    ← Subtitle-г видеонд шатаана

---
+++ merge_video.py
@@ -0,0 +1,7 @@
+import os
+
+def burn_subtitle(video_path, srt_path):
+    output_path = 'output/final_video.mp4'
+    command = f'ffmpeg -y -i {video_path} -vf subtitles={srt_path} {output_path}'
+    os.system(command)
+    return output_path
